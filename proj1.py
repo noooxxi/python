83781e621d9a7276dfa3d173502bfbb913e5f8b9
noxxi <noxxi@protonmail.com>
2018-03-19 20:38:44 +0100

    added project
    added
    + proj1.py

---
+++ proj1.py
@@ -0,0 +1,20 @@
+import random
+import sys
+import os
+
+
+
+str1 = "funny life hack"
+str2 = "you guys be the judge"
+
+
+var1 = 0
+var2 = 42
+
+
+
+print("watch this")
+print(var1)
+print("dont know about the if tags but if the above displayed a zero then it worked")
+print("ok so check this out im gonna combine var3 with the next string")
+print("whats up guys welcome back to my garage today we are doing a " + str1)
